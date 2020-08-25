e8328a7bf076a1b0fa6c1a0d2987fcd84911129f
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-25 19:35:54 -0400

    made config.js file in data directory, started setting up scheme-model.js

---
+++ schemes/scheme-model.js
@@ -0,0 +1,9 @@
+// make our db object by importing the config file
+// then we can use the db in our helper functions
+// we can access the database using db
+const db = require("../data/config");
+
+// define db helper functions here
+
+// export the db helper functions
+module.exports = {};
